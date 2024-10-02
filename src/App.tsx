7c6909ba000cf0ed3e3cc5f7c366efb763f9bef1
Bishal koirala <bishal123koirala@gmail.com>
2024-10-02 20:43:16 +0545

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 const  App = () => {
-  return <h1 className="text-black border-4 border-primary">Hello world!</h1>;
+  return(<>
+    <h1 className="text-3xl decoration-wavy underline leading-normal">Title 1</h1>
+    <h1 className="text-2xl">Title 2</h1>
+    <h1 className="text-xl">Title 3</h1>
+    <p className="text-base line-through">A regular Paragraph</p>
+    <p className="text-sm underline">A description paragraph</p>
+    <p className="text-xs italic">A little note</p>
+  </>);
+  
 }
 
 export default App;
