0f2aa7bdaaf8be83943df69e7f220ef1c22ee17e
Bishal koirala <bishal123koirala@gmail.com>
2024-10-06 10:19:00 +0545

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="flex flex-col Parent gap-2">
-      <div className="bg-slate-500">Header</div>
+      <div className="bg-z">Header</div>
       <div className=" flex subparent gap-2">
         <div className="basis-3/4 bg-cyan-50">Main content</div>
         <div className="basis-1/4 bg-cyan-600">sidebar</div>
