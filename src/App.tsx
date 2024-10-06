e4e9d12db7cdf68f3f99bc63834f6bdbd275d2a5
Bishal koirala <bishal123koirala@gmail.com>
2024-10-06 10:04:34 +0545

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-const  App = () => {
-  return(<>
-    <h1 className="text-3xl decoration-wavy underline leading-normal">Title 1</h1>
-    <h1 className="text-2xl">Title 2</h1>
-    <h1 className="text-xl">Title 3</h1>
-    <p className="text-base line-through">A regular Paragraph</p>
-    <p className="text-sm underline">A description paragraph</p>
-    <p className="text-xs italic">A little note</p>
-  </>);
-  
-}
+const App = () => {
+  return (
+    <div className="flex Parent gap-2">
+      <div>Header</div>
+      <div>Main content</div>
+      <div>sidebar</div>
+    </div>
+  );
+};
 
 export default App;
