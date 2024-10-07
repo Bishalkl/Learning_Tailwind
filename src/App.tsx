fe6ba7cbf90b0e57d0cf101b8b3dcf7154805a40
Bishal koirala <bishal123koirala@gmail.com>
2024-10-07 20:35:01 +0545

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 const App = () => {
   return (
-    <div className="Parent h-screen text-white bg-slate-900">
-      <div className="grid lg:grid-col-4 gap-2">
-        <div className="rounded-lg bg-blue-700 p-12">First column</div>
-        <div className="rounded-lg bg-blue-700 p-12">First column</div>
-        <div className="rounded-lg bg-blue-700 p-12">First column</div>
-        <div className="rounded-lg bg-blue-700 p-12">First column</div>
-        <div className="rounded-lg bg-blue-700 p-12">First column</div>
-        <div className="rounded-lg bg-blue-700 p-12">First column</div>
-      </div>
+    <div className="Parent h-screen text-white bg-slate-900 ">
+      
     </div>
   );
 };
