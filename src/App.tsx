afdd1fce5520e317cbdd421b2f820e8bb580a4ff
Bishal koirala <bishal123koirala@gmail.com>
2024-10-07 20:41:25 +0545

    done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div className="Parent h-screen text-white bg-slate-900 ">
       <input type="text" className="border-2 border-rose-600 outline-none" />
+      <button className="ring-4">Button</button>
     </div>
   );
 };
